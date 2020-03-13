542ad66ad03833aba3cd785d0fc99c4a3e7c6efc
Agnes Ketesdi <agnes.ketesdi@gmail.com>
2020-03-13 18:02:15 +0100

    updated plants route

--- server.js
+++ server.js
@@ -2,50 +2,62 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import bcrypt from 'bcrypt-nodejs'
 import crypto from 'crypto'
+import bcrypt from 'bcrypt-nodejs'
 
-
-// **** MONGOOSE SETUP ****
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/plantCare"
+//Setting up mongoDB database
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/plants'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// **** SETUP FOR UPLOADING IMAGES ****
-
-const multer = require("multer");
-const cloudinary = require("cloudinary");
-const cloudinaryStorage = require("multer-storage-cloudinary");
+const multer = require('multer')
+const cloudinary = require('cloudinary')
+const cloudinaryStorage = require('multer-storage-cloudinary')
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET
-});
+})
+
 const storage = cloudinaryStorage({
   cloudinary: cloudinary,
-  folder: "plant-project",
-  allowedFormats: ["jpg", "png"],
-  transformation: [{ width: 500, height: 500, crop: "limit" }]
-});
-const parser = multer({ storage: storage });
+  folder: 'plant-project',
+  allowedFormats: ['jpg', 'png'],
+  transformation: [{ width: 500, height: 500, crop: 'limit' }]
+})
 
-// **** MONGOOSE MODELS ****
+const parser = multer({ storage: storage })
 
-//Mongoose model for User
+const port = process.env.PORT || 8000
+const app = express()
 
-export const User = mongoose.model('User', {
+// Add middlewares to enable cors and json body parsing
+app.use(cors())
+app.use(bodyParser.json())
+
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header('Authorization') })
+  if (user) {
+    req.user = user
+    next()
+  } else {
+    res.status(401).json({ loggedOut: true })
+  }
+}
+
+// MODELS
+const User = mongoose.model('User', {
   name: {
     type: String,
     unique: true,
     required: true,
-    minlength: 3,
+    minlength: 3
   },
   email: {
     type: String,
     unique: true,
-    required: true,
+    required: true
   },
   password: {
     type: String,
@@ -55,11 +67,9 @@ export const User = mongoose.model('User', {
     type: String,
     default: () => crypto.randomBytes(128).toString('hex')
   }
-});
+})
 
-// Mongoose model for plant profile
-
-export const Plant = mongoose.model('Plant', {
+const Plant = mongoose.model('Plant', {
   name: {
     type: String,
     required: true,
@@ -73,19 +83,19 @@ export const Plant = mongoose.model('Plant', {
   acquiredAt: {
     type: Date,
     default: Date.now,
-    required: true
+    required: false
   },
   type: {
     type: String,
-    required: true
+    required: false
   },
   notes: {
     type: String,
-    required: true,
+    required: false,
     maxlength: 150
   },
   image: {
-    type: String,
+    type: String
   },
   waterAt: {
     type: Date,
@@ -93,86 +103,41 @@ export const Plant = mongoose.model('Plant', {
   }
 })
 
-// **** MIDDLEWARES ****
-
-// Middleware to check accesstokens
-
-const authenticateUser = async (req, res, next) => {
-  const user = await User.findOne({ accessToken: req.header('Authorization') });
-  if (user) {
-    req.user = user;
-    next();
-  } else {
-    res.status(401).json({ loggedOut: true });
-  }
-}
-
-// **** PORT SETUP ****
-const port = process.env.PORT || 8000
-const app = express()
-
-
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-
-// Post route for plant profiles
-app.post('/plants', parser.single("image"), (req, res) => {
-
-  const { name, location, acquiredAt, type, notes, waterAt } = req.body
-  // using mongoose model to create the database entry
-  const plant = new Plant({ name, location, acquiredAt, type, notes, waterAt })
-  plant.image = req.file.url
-  try {
-    //Success
-    const savedPlant = plant.save()
-    res.status(201).json(savedPlant)
-  } catch (err) {
-    // Failed
-    res.status(400).json({ message: 'Could not create plant profile', error: err.errors })
-  }
-})
-
-app.use(bodyParser.json())
-
-// **** ROUTES ****
-
+// Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Final Project - Backend by Agnes Ketesdi @Technigo 2020')
 })
 
-// **** USER ROUTES ****
-
-// Register a new user
-
-app.post("/users", async (req, res) => {
+// Create user
+app.post('/users', async (req, res) => {
   try {
-    const { name, email, password } = req.body;
-    const user = new User({ name, email, password: bcrypt.hashSync(password) });
-    await user.save();
-    res.status(201).json({ id: user._id, accessToken: user.accessToken });
+    const { name, email, password } = req.body
+    const user = new User({
+      name,
+      email,
+      password: bcrypt.hashSync(password),
+      lists
+    })
+    const saved = await user.save()
+    res.status(201).json(saved)
   } catch (err) {
-    res.status(400).json({
-      message: "Could not create user. Please try again!",
-      errors: err.errors
-    });
+    res
+      .status(400)
+      .json({ message: 'Could not create user', errors: err.errors })
   }
-});
+})
 
-// Secrets
-
-app.get('/secrets', authenticateUser);
-app.get('/secrets', (req, res) => {
-  res.json({ secret: 'This is a top secret message!' });
-});
-
-// Login
-
+// Login user
 app.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body
     const user = await User.findOne({ email })
     if (user && bcrypt.compareSync(password, user.password)) {
-      res.status(201).json({ name: user.name, userId: user._id, accessToken: user.accessToken })
+      res.status(201).json({
+        name: user.name,
+        userId: user._id,
+        accessToken: user.accessToken
+      })
     } else {
       res.json({ notFound: true })
     }
@@ -181,57 +146,75 @@ app.post('/login', async (req, res) => {
   }
 })
 
+// Create plant
+app.post('/plants', parser.single('image'), async (req, res) => {
+  try {
+    const { name, location, acquiredAt, type, notes, waterAt } = req.body
+    const plant = new Plant({
+      name,
+      location,
+      acquiredAt,
+      type,
+      notes,
+      waterAt
+    })
+    const savedPlant = await plant.save()
+    res.status(201).json(savedPlant)
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: 'Could not create plant', errors: err.errors })
+  }
+})
+
+app.get('/secrets', authenticateUser)
+app.get('/secrets', (req, res) => {
+  res.json({ secret: 'This is a top secret message!' })
+})
+
 // **** PLANT PROFILE ROUTES ****
 
 // get route for specific plant id
-app.get('/plants/:id', async (req, res) => {
-  const plant = await Plant.findById(req.params.id)
-  if (plant) {
-    res.json(plant)
-  } else {
-    res.status(404).json({ error: 'Plant not found' })
-  }
-})
+// app.get('/plants/:id', async (req, res) => {
+//   const plant = await Plant.findById(req.params.id)
+//   if (plant) {
+//     res.json(plant)
+//   } else {
+//     res.status(404).json({ error: 'Plant not found' })
+//   }
+// })
 
-// Put route for specific plant id
-app.put('/plants/:id', async (req, res) => {
-  const { id } = req.params
-  try {
-    //Success
-    await Plant.findOneAndUpdate({ '_id': id }, req.body, { new: true })
-    res.status(201).json()
-  } catch (err) {
-    // Failed
-    res.status(400).json({ message: 'Could not update plant profile', error: err.errors })
-  }
-})
+// // Put route for specific plant id
+// app.put('/plants/:id', async (req, res) => {
+//   const { id } = req.params
+//   try {
+//     //Success
+//     await Plant.findOneAndUpdate({ _id: id }, req.body, { new: true })
+//     res.status(201).json()
+//   } catch (err) {
+//     // Failed
+//     res
+//       .status(400)
+//       .json({ message: 'Could not update plant profile', error: err.errors })
+//   }
+// })
 
-// Delete route for specific plant id
-app.delete('/plants/:id', async (req, res) => {
-  const { id } = req.params
-  try {
-    // Success to delete the plant 
-    await Plant.findOneAndDelete({ '_id': id })
-    res.status(201).json()
-  } catch (err) {
-    // Failed
-    res.status(404).json({ message: 'Could not delete plant profile', error: err.errors })
-  }
-})
-
+// // Delete route for specific plant id
+// app.delete('/plants/:id', async (req, res) => {
+//   const { id } = req.params
+//   try {
+//     // Success to delete the plant
+//     await Plant.findOneAndDelete({ _id: id })
+//     res.status(201).json()
+//   } catch (err) {
+//     // Failed
+//     res
+//       .status(404)
+//       .json({ message: 'Could not delete plant profile', error: err.errors })
+//   }
+// })
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
-
-// Error handling
-
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.send(err);//this or res.status(err.status || 500).send('error')
-});
